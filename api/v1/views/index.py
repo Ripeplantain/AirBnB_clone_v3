d7bd2214b05daa5c7b0fd1d6d03ada9f7368448d
Ripeplantain <irvingmanny@gmail.com>
2023-01-22 19:41:13 +0000

    Revert "pycode api"
    This reverts commit 2c95444fcde3ed8c74fb39f6feb8c55abc3c3a06.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,7 +10,6 @@ def stat_return():
     """ return json status: OK """
     return jsonify({"status": "OK"})
 
-
 @app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def stat_count():
     count_data = {
